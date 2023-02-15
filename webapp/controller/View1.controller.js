23266907a7f3f23532b8ef762c7d8973c0fd0ff0
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-02-16 03:45:31 +0530

    Updated

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -8,29 +8,34 @@ sap.ui.define([
 		onInit : function(){
 			
 			//Step 1) Declare a brand new model.
-			var oModel = new sap.ui.model.json.JSONModel();
+			// var oModel = new sap.ui.model.json.JSONModel();
 			
 			//Step 2) Set the Data to the model.
-			oModel.setData({
-				//Without Array Data
-				// "Student":{					//"Student" = Entity Set / Table Name
-				// 	"name":"Kurvesh",		//"name" = Property / Key     |    "Kurvesh" = Value
-				// 	"no":"20012012037",
-				// 	"sem":8
-				// }
+			// // oModel.setData({
+			// 	// "student":{
+			// 	// 	"id":101,
+			// 	// 	"name":"Kurvesh",
+			// 	// 	"company":"Apple",
+			// 	// 	"age":24
+			// 	// }
 				
-				//With Array Data
-				"Student":{
-					"CE":[
-						{"name":"Hirwa","no":"20012012011","sem":5},
-						{"name":"Jay","no":"20012012035","sem":8}
-						],
-					"IT":[
-						{"name":"Siddhi","no":"20012012003","sem":2},
-						{"name":"Sharad","no":"20012012041","sem":1}
-						]
-				}
-			});
+			// 	//We use array
+			// 	// "student":{
+			// 	// 	"CE":[{
+			// 	// 		"id":102,
+			// 	// 		"name":"kurvesh",
+			// 	// 		"company":"Apple",
+			// 	// 		"age":24
+			// 	// 	},{
+			// 	// 		"id":103,
+			// 	// 		"name":"jay",
+			// 	// 		"company":"Google",
+			// 	// 		"age":23
+			// 	// 	}
+			// 	// 		]
+			// 	// }
+			// // });
+			// oModel.loadData("model/student.json");
 			
 			//Step 3) Set the model to your app / view / controll level.
 			
@@ -38,7 +43,7 @@ sap.ui.define([
 			//sap.ui.getCore().setModel(oModel);
 			
 			//View Level
-			this.getView().setModel(oModel);
+			this.getView().setModel();
 			
 			//Controll Level
 			//this.getView().byId("").setModel();
